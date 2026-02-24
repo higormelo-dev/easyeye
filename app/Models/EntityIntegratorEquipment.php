059b93fb38627c5867765e503724331b11a5f658
Higor Melo <higor_ap89@icloud.com>
2026-02-23 23:36:20 -0300

    Alteração para maiusculo mac e número de série

--- app/Models/EntityIntegratorEquipment.php
+++ app/Models/EntityIntegratorEquipment.php
@@ -91,7 +91,7 @@ class EntityIntegratorEquipment extends Model
      */
     public function setAttribute($key, $value): mixed
     {
-        if (is_string($value) && in_array($key, $this->uppercaseFields, true)) {
+        if (is_string($value) && isset($this->uppercaseFields) && in_array($key, $this->uppercaseFields, true)) {
             $value = mb_convert_case($value, MB_CASE_UPPER, 'UTF-8');
         }
 
