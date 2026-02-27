7940307c503b707db30075b4c0ef68cd95b7bd94
Higor Melo <higor_ap89@icloud.com>
2026-02-26 22:03:18 -0300

    Ajuste dos exames para pacientes

--- app/Http/Controllers/Api/PatientExamsController.php
+++ app/Http/Controllers/Api/PatientExamsController.php
@@ -32,11 +32,11 @@ class PatientExamsController extends Controller
     public function index(string $patientId)
     {
         $integrator   = request()->attributes->get('integrator');
-		dd($integrator->user->entity_id);
-        $patientExams = $this->model->query()
+		$patientExams = $this->model->query()
             ->with('patient', 'doctor', 'schedule', 'patient.person', 'doctor.person')
-            ->whereHas('patient', function ($query) use ($integrator) {
-                $query->where('entity_id', $integrator->user->entity_id);
+            ->whereHas('patient', function ($query) use ($integrator, $patientId) {
+                $query->where('id', $patientId)
+	                ->where('entity_id', $integrator->user->entity_id);
             });
 
         if (request()->has('search')) {
